b7dff025a36a8d7ff19bffae405887c5c28db39e
strvworks <strv13570@gmail.com>
2020-10-07 05:28:15 +0000

    update

--- src/client-entry.js
+++ src/client-entry.js
@@ -1,15 +1,8 @@
-// import FooBarPreRenderInterceptor from './client/js/util/Interceptor/FooBarPreRenderInterceptor';
-// import FooBarPostRenderInterceptor from './client/js/util/Interceptor/FooBarPostRenderInterceptor';
-import DefineSection from 'growi-plugin-section-ref/src/client/js/util/PreProcessor/DefineSection';
-import PreReferenceSection from 'growi-plugin-section-ref/src/client/js/util/PreProcessor/PreReferenceSection';
-import ReferenceSection from 'growi-plugin-section-ref/src/client/js/util/PreProcessor/ReferenceSection';
+import DefineSection from './client/js/util/PreProcessor/DefineSection';
+import PreReferenceSection from './client/js/util/PreProcessor/PreReferenceSection';
+import ReferenceSection from './client/js/util/PreProcessor/ReferenceSection';
 
 export default (appContainer) => {
-  // add interceptors
-  // appContainer.interceptorManager.addInterceptors([
-    // new FooBarPreRenderInterceptor(),
-    // new FooBarPostRenderInterceptor(appContainer),
-  // ]);
   appContainer.interceptorManager.addInterceptor(new PreReferenceSection());
   appContainer.interceptorManager.addInterceptor(new ReferenceSection());
   appContainer.originRenderer.preProcessors.unshift(new DefineSection());
